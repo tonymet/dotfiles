0a64294a00e69d74365b0fb8735fba927ba72ff3
Anthony Metzidis <anthonymetzidis@AMetzidis-MacBook-Pro-C02PX10VG8WL.local>
2016-06-01 17:54:44 -0700

    more tt-instances

--- tt/aws.zsh
+++ tt/aws.zsh
@@ -5,11 +5,18 @@ function tt-instances(){
 		"dev/cn")
 			aws --profile=tigertext.tonym ec2 describe-instances --filters 'Name=tag:Services,Values=*cn*' 'Name=tag:Environment,Values=development'|jq '.Reservations[].Instances[].PublicDnsName'
 		;;
+		"dev/paging")
+			aws --profile=tigertext.tonym ec2 describe-instances --filters 'Name=tag:Services,Values=*pag*' 'Name=tag:Environment,Values=development'|jq '.Reservations[].Instances[].PublicDnsName'
+		;;
+		"prod/paging")
+			aws --profile=tigertext.tonym ec2 describe-instances --filters 'Name=tag:Services,Values=*pag*' 'Name=tag:Environment,Values=production'|jq '.Reservations[].Instances[].Tags[]|select(.Key | contains("Name"))|.Value';
+		;;
 		"prod/cn")
-			aws --profile=tigertext.tonym ec2 describe-instances --filters 'Name=tag:Services,Values=*cn*' 'Name=tag:Environment,Values=production'|jq '.Reservations[].Instances[]'
+			aws --profile=tigertext.tonym ec2 describe-instances --filters 'Name=tag:Services,Values=*cn*' 'Name=tag:Environment,Values=production'|jq '.Reservations[].Instances[].Tags[]|select(.Key | contains("Name"))|.Value';
 		;;
 		*)
 			echo "we don't know nothing about $1"
+			return -1
 		;;
 	esac
 }
