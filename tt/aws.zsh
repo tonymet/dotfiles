942b47ec5552b8971e784705dcc90c44bbd24893
Anthony Metzidis <anthonymetzidis@AMetzidis-MacBook-Pro-C02PX10VG8WL.local>
2016-06-06 10:05:34 -0700

    add tt-version

--- tt/aws.zsh
+++ tt/aws.zsh
@@ -24,15 +24,32 @@ function tt-instances(){
 # list endpoints
 #
 function tt-endpoints(){
-	case $1 in 
+	case $1 in
 		"all")
 			aws --profile=tigertext.$USER route53 list-resource-record-sets --hosted-zone-id Z9I8ODW49EQEN|jq '.ResourceRecordSets[].Name'
 		;;
-		"dev")
-		aws --profile=tigertext.$USER route53 list-resource-record-sets --hosted-zone-id Z9I8ODW49EQEN|jq '.ResourceRecordSets[]|select(.Name|contains("dev"))|.Name'
+		*)
+			aws --profile=tigertext.$USER route53 list-resource-record-sets --hosted-zone-id Z9I8ODW49EQEN|jq ".ResourceRecordSets[]|select(.Name|contains(\"$1\"))|.Name"
+			return -1
+		;;
+	esac
+}
+
+
+function tt-version(){
+	case $1 in
+		"js_sdk")
+			for n in qa uat dev stag; do
+				echo $n ; curl -sL https://$n-sdk.tigertext.me/v1/sdk.js|egrep 'build-number'|head -n 1
+			done
+		;;
+		"web-console")
+			for n in qa uat dev stag; do
+				echo $n ; curl -s https://$n-console.tigertext.me/git.info
+			done
 		;;
 		*)
-			echo "we don't know nothing about $1"
+			echo "we dont' know nothing about $1"
 			return -1
 		;;
 	esac
