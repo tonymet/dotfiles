87ca9f98e90e5ad0bc497562942ca25e0610ffee
Anthony Metzidis <anthonymetzidis@AMetzidis-MacBook-Pro-C02PX10VG8WL.local>
2016-06-23 14:31:05 -0700

    add rman & fib

--- misc/functions.zsh
+++ misc/functions.zsh
@@ -3,3 +3,29 @@
 function wanip(){
   dig +short myip.opendns.com @resolver1.opendns.com
 }
+
+
+# fibonacci
+function fib() {
+	local a c
+	local -F1 b
+	a=0 ; b=1
+	print $a
+	repeat 100
+	do
+		print "${b%.*}"
+		c=$a
+		a=$b
+		((b = c + b))
+	done
+}
+
+function rman(){
+  docs=~/tigertext/lib/redis-doc/commands
+  if [[ ! -f $docs/$1.md ]]; then
+    echo "$1 command not found. "
+    ls $docs
+    return -1
+  fi
+  ronn -m $docs/$1.md
+}
